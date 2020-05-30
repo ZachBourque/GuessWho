d0535f1984a0f01a3ba789160c47179ec5355fd6
Zach Bourque <3ternalz@gmail.com>
2020-05-30 15:22:16 -0700

    Initial commit of the first version of the website. This git repository will mostly be used to transfer files between this directory and the filebase in the Google VM

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+/*!
+    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 71)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 80
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Floating label headings for the contact form
+  $(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
+
+})(jQuery); // End of use strict
+
+var chosen = false;
+
+function togglePhoto(elem) {
+  if (chosen) {
+    var id = $(elem).attr("id");
+    $(`#${id}`).toggleClass("greyed");
+  } else {
+    var elemCopy = elem.cloneNode(true);
+
+    var el = $( '<div></div>' );
+    el.html(elemCopy);
+
+    document.documentElement.scrollTop = 0;
+    $(elemCopy).removeClass();
+    $(elemCopy).removeAttr('id');
+    $(elemCopy).addClass('character');
+    $(elemCopy).prop("onclick", null).off("click");
+    $(elemCopy).css('border', '0px');
+    $("#chosenCharacter").css('margin', 'auto');
+    $('#chosenCharacter').html(elemCopy);
+    $('#headerText').html("YOUR CHARACTER:");
+    $('#chooseCharacter').css('background-color', "#f5dfda");
+  }
+  chosen = true;
+}
+
